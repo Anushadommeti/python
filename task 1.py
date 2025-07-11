34d260afaaef16d64e3fa533e95c8c999f00ea42
Anushadommeti <dommetianusha9@gmail.com>
2025-07-11 19:30:32 +0530

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,30 @@
+ #simple interest calculations
+p=5
+t=6
+r=2
+s=(p*t*r)/100
+print(s)
+
+#this code converts celsius to fahrenheit using the formula
+c=40
+f=9/5*c+32
+print(f)
+
+#average of three numbers
+a=34
+b=44
+c=50
+A=(a+b+c)/3
+print(A)
+
+#area of circle
+a=12
+A=(22/7)*a*a
+print(A)
+
+#Swap two varables
+a=10
+b=25
+print(a,b)
+a,b=b,a
+print(a,b)
